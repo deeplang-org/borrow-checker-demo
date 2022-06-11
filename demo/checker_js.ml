92d1f3bd249891e4455e030d7965fb67a3c65c29
Guest0x0 <guest0x0@outlook.com>
2022-06-11 20:07:59 +0800

    allow HTML demo to select builtin examples

--- demo/checker_js.ml
+++ demo/checker_js.ml
@@ -15,6 +15,24 @@ let _ =
     let open Js_of_ocaml in
     Js.export "checker"
         (object%js
+            method exampleNames =
+                Examples.examples
+                |> Array.map (fun (name, _, _) -> Js.string name)
+                |> Js.array
+
+            method getExample name =
+                let name = Js.to_string name in
+                let rec find i =
+                    if i >= Array.length Examples.examples
+                    then Js.null
+                    else
+                        let (name', src, _) = Examples.examples.(i) in
+                        if name' = name
+                        then Js.some (Js.string src)
+                        else find (i + 1)
+                in
+                find 0
+
             method run margin src =
                 let fmt = Format.str_formatter in
                 Format.pp_set_margin fmt margin;
